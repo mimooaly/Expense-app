5968eba1787a75254d0599d52cc101126f7cfb1d
Mimo Aly <mhassa@gmail.com>
2025-05-13 05:58:26 +0800

    fixing security issues

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -18,13 +18,9 @@ const GoogleAnalytics = () => {
     script1.src = "https://www.googletagmanager.com/gtag/js?id=G-EPEQHD72D7";
     document.head.appendChild(script1);
 
+    // Load initialization script from external file
     const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-EPEQHD72D7');
-    `;
+    script2.src = "/analytics.js";
     document.head.appendChild(script2);
 
     return () => {
